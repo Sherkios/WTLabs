874952513791216575ddeb6898afd66906ded694
Grishchenko Igor <Sherkiosp@gmail.com>
2022-05-19 23:02:48 +0500

    5 лабораторная работа

--- dist/connectBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASS", "");
-define("DB", "ajax"); // установление соединения с сервером
-// подключение к базе данных:
-$mysqli = mysqli_connect(HOST, USER, PASS, DB) or die("Невозможно
-	подключиться к серверу");
